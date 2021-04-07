b27c727bb498a764fc73ef7aa00869996f04c44c
Mubelotix <mubelotix@gmail.com>
2021-04-07 09:32:30 +0200

    Implement SpawnLivingEntity

--- src/packets/play_clientbound.rs
+++ src/packets/play_clientbound.rs
@@ -13,7 +13,7 @@ pub struct SpawnEntity {
     pub data: i32,
     pub velocity_x: i16,
     pub velocity_y: i16,
-    pub velocity_z: i16
+    pub velocity_z: i16,
 }
 
 #[derive(Debug, MinecraftPacket)]
@@ -23,4 +23,19 @@ pub struct SpawnExperienceOrb {
     pub y: f64,
     pub z: f64,
     pub count: i16,
+}
+
+pub struct SpawnLivingEntity {
+    pub id: VarInt,
+    pub uuid: UUID,
+    pub entity_type: VarInt,
+    pub x: f64,
+    pub y: f64,
+    pub z: f64,
+    pub yaw: Angle,
+    pub pitch: Angle,
+    pub head_pitch: Angle,
+    pub velocity_x: i16,
+    pub velocity_y: i16,
+    pub velocity_z: i16,
 }
