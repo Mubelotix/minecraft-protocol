50f7b39ae9ada09c9f60c555d88c3a9b6c9cb61d
Mubelotix <mubelotix@gmail.com>
2021-04-07 14:38:33 +0200

    Implement BlockBreakAnimation

--- src/packets/play_clientbound.rs
+++ src/packets/play_clientbound.rs
@@ -80,3 +80,18 @@ pub struct AcknowledgePlayerDigging {
     pub status: crate::digging_states::PartialDiggingState,
     pub successful: bool,
 }
+
+/// 0–9 are the displayable destroy stages and each other number means that there is no animation on this coordinate.
+/// 
+/// Block break animations can still be applied on air; the animation will remain visible although there is no block being broken. However, if this is applied to a transparent block, odd graphical effects may happen, including water losing its transparency. (An effect similar to this can be seen in normal gameplay when breaking ice blocks)
+/// 
+/// If you need to display several break animations at the same time you have to give each of them a unique Entity ID. The entity ID does not need to correspond to an actual entity on the client. It is valid to use a randomly generated number.
+#[derive(Debug, MinecraftPacket)]
+pub struct BlockBreakAnimation {
+    /// Entity ID of the entity breaking the block
+    pub id: VarInt,
+    /// Block Position
+    pub location: Position,
+    /// 0–9 to set it, any other value to remove it
+    pub destroy_stage: u8,
+}
