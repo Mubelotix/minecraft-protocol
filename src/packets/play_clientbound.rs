8dab7b3be1e2a45ef43e66e8cffea18ea155d4be
Mubelotix <mubelotix@gmail.com>
2021-04-08 10:13:34 +0200

    Implement CloseWindow

--- src/packets/play_clientbound.rs
+++ src/packets/play_clientbound.rs
@@ -198,3 +198,9 @@ pub struct WindowConfirmation {
     /// Whether the action was accepted.
     pub accepted: bool,
 }
+
+/// This packet is sent from the server to the client when a window is forcibly closed, such as when a chest is destroyed while it's open.
+pub struct CloseWindow {
+    /// This is the ID of the window that was closed. 0 for inventory.
+    pub window_id: u8,
+}
