c678564ff0616d1117ce2131e3008b2627d7a2cc
Mubelotix <mubelotix@gmail.com>
2021-04-09 11:27:22 +0200

    Implement TimeUpdate

--- src/packets/play_clientbound.rs
+++ src/packets/play_clientbound.rs
@@ -828,4 +828,16 @@ pub enum ClientBoundPacket<'a> {
         entity_name: &'a str,
         score_action: crate::teams::ScoreboardScoreAction<'a>,
     },
+
+    /// Time is based on ticks, where 20 ticks happen every second.
+    /// There are 24000 ticks in a day, making Minecraft days exactly 20 minutes long.
+    /// The time of day is based on the timestamp modulo 24000. 0 is sunrise, 6000 is noon, 12000 is sunset, and 18000 is midnight.
+    /// The default SMP server increments the time by 20 every second.
+    TimeUpdate {
+        /// In ticks; not changed by server commands
+        world_age: i64,
+        /// The world (or region) time, in ticks.
+        /// If negative the sun will stop moving.
+        time_of_day: i64,
+    },
 }
