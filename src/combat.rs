bb8ac0ae632dca3ed29bda159a594ac35ef666d9
Mubelotix <mubelotix@gmail.com>
2021-04-12 00:14:43 +0200

    Fix bug

--- src/combat.rs
+++ src/combat.rs
@@ -8,13 +8,13 @@ pub enum CombatEvent<'a> {
         /// Length of the combat in ticks
         duration: VarInt,
         /// ID of the primary opponent of the ended combat, or -1 if there is no obvious primary opponent
-        opponent_entity_id: VarInt,
+        opponent_entity_id: i32,
     },
     EntityDead {
         /// Entity ID of the player that died (should match the client's entity ID)
         dead_entity_id: VarInt,
         /// The killing entity's ID, or -1 if there is no obvious killer
-        killer_entity_id: VarInt,
+        killer_entity_id: i32,
         death_message: Chat<'a>,
     },
 }
