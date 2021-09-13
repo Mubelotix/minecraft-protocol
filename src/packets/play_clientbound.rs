89ae51b5bc4588c9280b0013a4024f8fa2030913
Mubelotix <mubelotix@gmail.com>
2021-09-13 17:53:15 +0200

    Add SetTitleSubTitle

--- src/packets/play_clientbound.rs
+++ src/packets/play_clientbound.rs
@@ -942,6 +942,10 @@ pub enum ClientboundPacket<'a> {
         score_action: teams::ScoreboardScoreAction<'a>,
     },
 
+    SetTitleSubTitle {
+        subtitle_text: Chat<'a>,
+    },
+
     /// Time is based on ticks, where 20 ticks happen every second.
     /// There are 24000 ticks in a day, making Minecraft days exactly 20 minutes long.
     /// The time of day is based on the timestamp modulo 24000. 0 is sunrise, 6000 is noon, 12000 is sunset, and 18000 is midnight.
