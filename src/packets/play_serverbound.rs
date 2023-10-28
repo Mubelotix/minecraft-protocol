b23b6491a5362bd18eeae70a9772f54eaebfffc2
Mubelotix <mubelotix@gmail.com>
2023-10-29 00:24:56 +0200

    Update SetPlayerOnGround

--- src/packets/play_serverbound.rs
+++ src/packets/play_serverbound.rs
@@ -266,11 +266,13 @@ pub enum ServerboundPacket<'a> {
     /// This packet is used to indicate whether the player is on ground (walking/swimming), or airborne (jumping/falling).
     ///
     /// Vanilla clients will send Player Position once every 20 ticks even for a stationary player.
+    /// 
+    /// This packet is used to indicate whether the player is on ground (walking/swimming), or airborne (jumping/falling).
     ///
     /// When dropping from sufficient height, fall damage is applied when this state goes from false to true.
     /// The amount of damage applied is based on the point where it last changed from true to false.
     /// Note that there are several movement related packets containing this state.
-    PlayerFulcrum {
+    SetPlayerOnGround {
         /// `true` if the client is on the ground, `false` otherwise
         on_ground: bool,
     },
