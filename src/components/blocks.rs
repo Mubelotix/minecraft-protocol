29202254b0c2cea714e90682ad1a0b218dcae72a
DimitriTimoz <dimitri.timoz@orange.fr>
2023-10-28 22:43:45 +0200

    remove BlockEntityDataAction

--- src/components/blocks.rs
+++ src/components/blocks.rs
@@ -32,39 +32,6 @@ pub enum BlockFace {
     East,
 }
 
-/// The type of update to perform used in [crate::packets::play_clientbound::BlockEntityData] packets.
-#[minecraft_enum(u8)]
-#[derive(Debug)]
-pub enum BlockEntityDataAction {
-    /// Set data of a mob spawner (everything except for SpawnPotentials: current delay, min/max delay, mob to be spawned, spawn count, spawn range, etc.)
-    MobSpawner = 1,
-    /// Set command block text (command and last execution status)
-    CommandBlock,
-    /// Set the level, primary, and secondary powers of a beacon
-    Beacon,
-    /// Set rotation and skin of mob head
-    MobHead,
-    /// Declare a conduit
-    Conduit,
-    /// Set base color and patterns on a banner
-    Banner,
-    /// Set the data for a Structure tile entity
-    Structure,
-    /// Set the destination for a end gateway
-    EndGateway,
-    /// Set the text on a sign
-    Sign,
-
-    /// Declare a bed
-    Bed = 11,
-    /// Set data of a jigsaw block
-    Jigsaw,
-    /// Set items in a campfire
-    Campfire,
-    /// Beehive information
-    Beehive,
-}
-
 #[derive(Debug, MinecraftPacketPart)]
 pub struct MultiBlockChange<'a> {
     /// Chunk section coordinate (encoded chunk x and z with each 22 bits, and section y with 20 bits, from left to right).
