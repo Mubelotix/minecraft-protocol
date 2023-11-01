e38ebc0c9b8b413b5f2ba6e1fc5f4d4b7ec4e8bf
DimitriTimoz <dimitri.timoz@orange.fr>
2023-11-01 19:03:49 +0100

    healthComponent change struct

--- minecraft-server/src/ecs/components/health.rs
+++ minecraft-server/src/ecs/components/health.rs
@@ -6,38 +6,45 @@ pub struct HealthComponent {
     pub max_health: f32,
 }
 
+impl HealthComponent {
+    /// Returns true if the entity is dead.s
+    pub async fn is_dead(&self) -> bool {
+        self.health <= 0.0
+    }
+
+    pub async fn get_health(&self) -> f32{
+        self.health
+    }
+
+    /// Heals the entity by the given amount.
+    pub async fn heal(&mut self, amount: f32) {
+        self.health = (self.health + amount).min(self.max_health);
+    }
+
+    /// Fully heals the entity.
+    pub async fn full_heal(&mut self) {
+        self.health = self.max_health;
+    }
+
+    /// Damages the entity by the given amount.
+    pub async fn damage(&mut self, amount: f32) -> Option<()> {
+        self.health -= amount;
+        self.health = self.health.max(0.0);
+        Some(())
+    }
+}
+
 impl Entities {
+    /// Set the health of an entity.
+    pub async fn set_health(&self, id: Eid, health: HealthComponent) -> Option<()> {
+        let mut health_components = self.health_components.write().await;
+        health_components.insert(id, health);
+        Some(())
+    }
+    
+    /// Get the health of an entity.
     pub async fn get_health(&self, id: Eid) -> Option<HealthComponent> {
         self.health_components.read().await.get(&id).cloned()
     }
 
-    /// Returns true if the entity is dead.
-    pub async fn is_dead(&self, id: Eid) -> Option<bool> {
-        self.get_health(id).await.map(|health| health.health <= 0.0)
-    }
-
-    /// Heals the entity by the given amount.
-    pub async fn heal(&self, id: Eid, amount: f32) -> Option<()> {
-        let mut health_components = self.health_components.write().await;
-        let health = health_components.get_mut(&id)?;
-        health.health = (health.health + amount).min(health.max_health);
-        Some(())
-    }
-
-    /// Fully heals the entity.
-    pub async fn full_heal(&self, id: Eid) -> Option<()> {
-        let mut health_components = self.health_components.write().await;
-        let health = health_components.get_mut(&id)?;
-        health.health = health.max_health;
-        Some(())
-    }
-
-    /// Damages the entity by the given amount.
-    pub async fn damage(&self, id: Eid, amount: f32) -> Option<()> {
-        let mut health_components = self.health_components.write().await;
-        let health = health_components.get_mut(&id)?;
-        health.health -= amount;
-        health.health = health.health.max(0.0);
-        Some(())
-    }
 }
