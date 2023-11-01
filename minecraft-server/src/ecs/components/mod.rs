e3a72b474f44859015383d7e1f9a5d48822c2c16
DimitriTimoz <dimitri.timoz@orange.fr>
2023-11-01 03:27:31 +0100

    some components

--- minecraft-server/src/ecs/components/mod.rs
+++ minecraft-server/src/ecs/components/mod.rs
@@ -1,10 +1,12 @@
+pub use super::*;
+
 pub mod health;
 pub mod positions;
 
 pub use health::HealthComponent;
 pub use positions::PositionComponent;
 
-#[derive(Clone)]
+#[derive(Clone, Eq, Hash, PartialEq)]
 pub enum Components {
     Health,
     Position,
--- minecraft-server/src/ecs/components/positions.rs
+++ minecraft-server/src/ecs/components/positions.rs
@@ -1,4 +1,12 @@
+use crate::prelude::*;
 
+#[derive(Clone)]
 pub struct PositionComponent {
+    
+}
 
+impl Entities {
+    pub async fn get_position(&self, id: Eid) -> Option<PositionComponent> {
+        self.position_components.read().await.get(&id).cloned()
+    }
 }
