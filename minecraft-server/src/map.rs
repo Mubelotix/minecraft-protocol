708d329f33aca0a0f64633165cd010b8d589aa43
Mubelotix <mubelotix@gmail.com>
2023-11-01 11:53:22 +0100

    Add loading

--- minecraft-server/src/map.rs
+++ minecraft-server/src/map.rs
@@ -1,5 +1,5 @@
 use std::collections::HashMap;
-use minecraft_protocol::components::chunk::Chunk;
+use minecraft_protocol::components::chunk::{Chunk, PalettedData};
 use tokio::sync::RwLock;
 use crate::prelude::*;
 
@@ -11,14 +11,38 @@ struct ChunkColumn {
     chunks: Vec<Chunk>,
 }
 
+impl ChunkColumn {
+    pub fn flat() -> Self {
+        let empty_chunk = Chunk {
+            block_count: 0,
+            blocks: PalettedData::Single { value: 0 },
+            biomes: PalettedData::Single { value: 4 },
+        };
+        let dirt_chunk = Chunk {
+            block_count: 4096,
+            blocks: PalettedData::Single { value: minecraft_protocol::ids::blocks::Block::GrassBlock.default_state_id() },
+            biomes: PalettedData::Single { value: 4 },
+        };
+        let mut chunks = Vec::new();
+        chunks.push(dirt_chunk);
+        for _ in 0..23 {
+            chunks.push(empty_chunk.clone());
+        }
+        ChunkColumn { chunks }
+    }
+}
+
 impl WorldMap {
     pub async fn load(&self, position: ChunkPosition) {
-        todo!()
+        let chunk = ChunkColumn::flat(); // TODO: load from disk
+        let mut chunks = self.chunks.write().await;
+        chunks.entry(position).or_insert_with(|| RwLock::new(chunk));
     }
 
     pub async fn unload(&self, position: ChunkPosition) {
         let mut chunks = self.chunks.write().await;
         chunks.remove(&position);
+        // TODO: write to disk
     }
 }
 
