3330dda582f0a4191846b1ff8e214cd3ed7c91e8
DimitriTimoz <dimitri.timoz@orange.fr>
2023-11-01 18:56:47 +0100

    impl PositionComponent basic

--- minecraft-server/src/ecs/components/positions.rs
+++ minecraft-server/src/ecs/components/positions.rs
@@ -9,4 +9,26 @@ impl Entities {
     pub async fn get_position(&self, id: Eid) -> Option<PositionComponent> {
         self.position_components.read().await.get(&id).cloned()
     }
+
+    pub async fn set_position(&self, id: Eid, position: PositionComponent) -> Option<()> {
+        let mut position_components = self.position_components.write().await;
+        position_components.insert(id, position);
+        Some(())
+    }
+}
+
+impl PositionComponent {
+    pub async fn set_position(&mut self, position: Position) -> Option<()> {
+        self.position = position;
+        Some(())
+    }
+
+    pub async fn get_position(&self) -> Position {
+        self.position.clone()
+    }
+
+    pub async fn move_entity(&mut self, delta: Position) -> Option<()> {
+        self.position += delta;
+        Some(())
+    }
 }
