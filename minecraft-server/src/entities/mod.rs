dcba19fe2fb515adc0cef9ef93036d3473c2d2bd
Mubelotix <mubelotix@gmail.com>
2023-11-02 17:54:39 +0100

    Add AnyEntity

--- minecraft-server/src/entities/mod.rs
+++ minecraft-server/src/entities/mod.rs
@@ -2,3 +2,24 @@ mod entity;
 pub use entity::*;
 mod thrown_item_projectile;
 pub use thrown_item_projectile::*;
+
+pub enum AnyEntity {
+    Entity(Entity),
+    ThrownItemProjectile(ThrownItemProjectile),
+}
+
+impl AnyEntity {
+    pub fn as_entity(&self) -> &Entity {
+        match self {
+            AnyEntity::Entity(entity) => entity,
+            AnyEntity::ThrownItemProjectile(throw_item_projectile) => throw_item_projectile.get_entity(),
+        }
+    }
+
+    pub fn as_thrown_item_projectile(&self) -> Option<&ThrownItemProjectile> {
+        match self {
+            AnyEntity::ThrownItemProjectile(throw_item_projectile) => Some(throw_item_projectile),
+            _ => None,
+        }
+    }
+}
