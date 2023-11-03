064a651886fd2a487f5b6a0bd34e24d5ac33c22c
Mubelotix <mubelotix@gmail.com>
2023-11-03 01:29:41 +0100

    Add Arrow

--- minecraft-server/src/entities/arrow.rs
+++ minecraft-server/src/entities/arrow.rs
@@ -1,5 +1,7 @@
 use super::*;
 
+#[derive(Default)]
+#[inheritable]
 #[inherit(Entity)]
 pub struct AbstractArrow {
     pub entity: Entity,
@@ -8,3 +10,17 @@ pub struct AbstractArrow {
     pub piercing_level: i32,
 }
 
+#[inherit(AbstractArrow, Entity)]
+pub struct Arrow {
+    pub abstract_arrow: AbstractArrow,
+    pub color: isize,
+}
+
+impl Default for Arrow {
+    fn default() -> Self {
+        Self {
+            abstract_arrow: AbstractArrow::default(),
+            color: -1,
+        }
+    }
+}
