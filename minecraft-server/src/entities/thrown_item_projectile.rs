98781192d8a5b083d78adcc420b9d5f24fc36465
Mubelotix <mubelotix@gmail.com>
2023-11-03 01:17:24 +0100

    Group in file

--- minecraft-server/src/entities/thrown_item_projectile.rs
+++ minecraft-server/src/entities/thrown_item_projectile.rs
@@ -15,3 +15,43 @@ impl Default for ThrownItemProjectile {
         }
     }
 }
+
+#[inherit(ThrownItemProjectile, Entity)]
+pub struct ThrownEgg {
+    pub thrown_item_projectile: ThrownItemProjectile,
+}
+
+impl Default for ThrownEgg {
+    fn default() -> Self {
+        ThrownEgg {
+            thrown_item_projectile: ThrownItemProjectile {
+                entity: Entity::default(),
+                item: Slot {item: Some(SlotItem {
+                    item_id: Item::Egg,
+                    item_count: 1,
+                    nbt_data: NbtTag::Null
+                })},
+            }
+        }
+    }
+}
+
+#[inherit(ThrownItemProjectile, Entity)]
+pub struct ThrownEnderPearl {
+    pub thrown_item_projectile: ThrownItemProjectile,
+}
+
+impl Default for ThrownEnderPearl {
+    fn default() -> Self {
+        ThrownEnderPearl {
+            thrown_item_projectile: ThrownItemProjectile {
+                entity: Entity::default(),
+                item: Slot {item: Some(SlotItem {
+                    item_id: Item::EnderPearl,
+                    item_count: 1,
+                    nbt_data: NbtTag::Null
+                })},
+            }
+        }
+    }
+}
