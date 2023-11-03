cdbe430672a5ca9609cdc0f0d38346aa9a9b2b74
Mubelotix <mubelotix@gmail.com>
2023-11-03 02:13:36 +0100

    Clean Entities

--- minecraft-server/src/ecs/entities.rs
+++ minecraft-server/src/ecs/entities.rs
@@ -10,10 +10,8 @@ pub struct Entities {
     /// A hashmap of chunk positions to get a list of entities in a chunk
     pub chunks: RwLock<HashMap<ChunkPosition, HashSet<Eid>>>,
     pub uuids: RwLock<HashMap<UUID, Eid>>,
-    pub entities_by_tag: RwLock<HashMap<Tag, HashSet<Eid>>>,
-
-    pub health_components: RwLock<HashMap<Eid, HealthComponent>>,
-    pub position_components: RwLock<HashMap<Eid, PositionComponent>>,
+    
+    // TODO: pub entities_by_tag: RwLock<HashMap<Tag, HashSet<Eid>>>,
 }
 
 impl Entities {
