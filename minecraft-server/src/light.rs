635cef58ffd9a0a66d14c23095636dfa1a823835
DimitriTimoz <dimitri.timoz@orange.fr>
2023-11-12 00:52:01 +0100

    Set Layer of light

--- minecraft-server/src/light.rs
+++ minecraft-server/src/light.rs
@@ -12,24 +12,30 @@ impl SectionLightData {
     }
 
     /// Get the light level at the given position.
-    pub fn get(&self, postion: BlockPositionInChunk) -> u8 {
+    pub fn get(&self, postion: BlockPositionInChunk) -> Result<u8, ()> {
         let (x, y, z) = (postion.bx as usize, postion.by as usize, postion.bz as usize);
-        let index = (y << 4) | (z << 4) | x;
+        let index = (y << 8) | (z << 4) | x;
         let byte_index = index >> 1;
 
-        assert!(byte_index < 2048);
+        if byte_index >= 2048 {
+            return Err(());
+        }
 
         if index & 1 == 0 {
-            self.0[byte_index] & 0x0F
+            Ok(self.0[byte_index] & 0x0F)
         } else {
-            (self.0[byte_index] & 0xF0) >> 4
+            Ok((self.0[byte_index] & 0xF0) >> 4)
         }
     }
 
     /// Set the light level at the given position.
-    pub fn set(&mut self, postion: BlockPositionInChunk, level: u8) {
+    pub fn set(&mut self, postion: BlockPositionInChunk, level: u8) -> Result<(), ()> {
+        if level > 15 {
+            return Err(());
+        }
+
         let (x, y, z) = (postion.bx as usize, postion.by as usize, postion.bz as usize);
-        let index = (y << 4) | (z << 4) | x;
+        let index = (y << 8) | (z << 4) | x;
         let byte_index = index >> 1;
 
         if index & 1 == 0 {
@@ -39,16 +45,11 @@ impl SectionLightData {
         }
     }
     
-    /// Set the light level at the given slice to the given level.
-    pub(super) fn set_slice(&mut self, slice: u8 , level: u8) {
-        let slice_index = (slice as usize) << 4;
-        let level_byte = level << 4 | level;
-        for z in 0..16 {
-            let z_index = slice_index | (z << 4);
-            for x in 0..8 {
-                let index = z_index | x;
-                self.0[index] = level_byte;
-            }
+    /// Set the light level at the given layer to the given level.
+    pub(super) fn set_layer(&mut self, layer: u8 , level: u8) -> Result<(), ()> {
+        if level > 15 {
+            return Err(());
+        }
         }
     }
 }
@@ -90,20 +91,44 @@ mod tests {
     fn test_section_light_data() {
         let mut data = SectionLightData::new();
 
-        data.set(BlockPositionInChunk { bx: 0, by: 0, bz: 0 }, 15);
-        assert_eq!(data.get(BlockPositionInChunk { bx: 0, by: 0, bz: 0 }), 15);
+        data.set(BlockPositionInChunk { bx: 0, by: 0, bz: 0 }, 15).unwrap();
+        assert_eq!(data.get(BlockPositionInChunk { bx: 0, by: 0, bz: 0 }).unwrap(), 15);
 
-        data.set(BlockPositionInChunk { bx: 0, by: 0, bz: 0 }, 0);
-        assert_eq!(data.get(BlockPositionInChunk { bx: 0, by: 0, bz: 0 }), 0);
+        data.set(BlockPositionInChunk { bx: 0, by: 0, bz: 0 }, 0).unwrap();
+        assert_eq!(data.get(BlockPositionInChunk { bx: 0, by: 0, bz: 0 }).unwrap(), 0);
 
-        data.set(BlockPositionInChunk { bx: 0, by: 0, bz: 1 }, 1);
-        assert_eq!(data.get(BlockPositionInChunk { bx: 0, by: 0, bz: 1 }), 1);
+        data.set(BlockPositionInChunk { bx: 0, by: 0, bz: 1 }, 1).unwrap();
+        assert_eq!(data.get(BlockPositionInChunk { bx: 0, by: 0, bz: 1 }).unwrap(), 1);
 
-        data.set(BlockPositionInChunk { bx: 0, by: 1, bz: 1 }, 15);
-        assert_eq!(data.get(BlockPositionInChunk { bx: 0, by: 1, bz: 1 }), 15);
+        data.set(BlockPositionInChunk { bx: 0, by: 1, bz: 1 }, 15).unwrap();
+        assert_eq!(data.get(BlockPositionInChunk { bx: 0, by: 1, bz: 1 }).unwrap(), 15);
 
-        data.set(BlockPositionInChunk { bx: 1, by: 1, bz: 1 }, 1);
-        assert_eq!(data.get(BlockPositionInChunk { bx: 1, by: 1, bz: 1 }), 1);
+        data.set(BlockPositionInChunk { bx: 1, by: 1, bz: 1 }, 1).unwrap();
+        assert_eq!(data.get(BlockPositionInChunk { bx: 1, by: 1, bz: 1 }).unwrap(), 1);
+
+        data.set(BlockPositionInChunk { bx: 2, by: 0, bz: 0 }, 1).unwrap();
+        assert_eq!(data.get(BlockPositionInChunk { bx: 2, by: 0, bz: 0 }).unwrap(), 1);
+
+        // Manual layer
+        for z in 0..16 {
+            for x in 0..16 {
+                data.set(BlockPositionInChunk { bx: x, by: 0, bz: z }, 15).unwrap();
+            }
+        }
+
+        for z in 0..16 {
+            for x in 0..16 {
+                assert_eq!(data.get(BlockPositionInChunk { bx: x, by: 0, bz: z }).unwrap(), 15, "x: {}, z: {}", x, z);
+            }
+        }
+
+        // Test layer
+        data.set_layer(1, 15).unwrap();
+        for x in 0..16 {
+            for z in 0..16 {
+                assert_eq!(data.get(BlockPositionInChunk { bx: x, by: 1, bz: z }).unwrap(), 15, "x: {}, z: {}", x, z);
+            }
+        }
 
     }
 }
